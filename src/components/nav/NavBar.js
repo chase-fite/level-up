a44edbdd287e35414e0872d4fb5f5840b1192ecd
Chase Fite <chasef7031@gmail.com>
2019-12-10 14:30:25 -0600

    finished display and some styling for workouts tab

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -7,23 +7,21 @@ class NavBar extends Component {
 
   render() {
     return (
-      <header>
-        <NavBarBS sticky="top">
-          <ul className="container">
-            <li><Link className="nav-link" to="/">Home</Link></li>
-            <li><Link className="nav-link" to="/workouts">Workouts</Link></li>
-            {(true) ?
-            <>
-              <li><Link className="nav-link" to="/exercises">Exercises</Link></li>
-              <li><Link className="nav-link" to="/history">History</Link></li>
-              <li><span className="nav-link">Logout</span></li>
-            </>
-            :
-            <li><Link className="nav-link" to="/login">Login</Link></li>
-            }
-          </ul>
-        </NavBarBS>
-      </header>
+      <NavBarBS sticky="top" className="navbar-style">
+        <ul className="container">
+          <li><Link className="nav-link" to="/">Home</Link></li>
+          <li><Link className="nav-link" to="/workouts">Workouts</Link></li>
+          {(true) ?
+          <>
+            <li><Link className="nav-link" to="/exercises">Exercises</Link></li>
+            <li><Link className="nav-link" to="/history">History</Link></li>
+            <li><span className="nav-link">Logout</span></li>
+          </>
+          :
+          <li><Link className="nav-link" to="/login">Login</Link></li>
+          }
+        </ul>
+      </NavBarBS>
     )
   }
 }
---
+++ src/components/workouts/Workout.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import { Card, Container, Row, Col } from 'react-bootstrap'
+import './Workouts.css'
+
+class Workout extends Component {
+    state = {
+        exercises: []
+    }
+
+    componentDidMount() {
+        const tempArray = this.props.workout.exercises.map(x => x)
+        this.setState({
+            exercises: tempArray
+        })
+    }
+
+    render() {
+        return (
+            <Card className="workout-card border-primary mb-3">
+                <Card.Header className="text-center">{this.props.workout.name}</Card.Header>
+                <Container className="con-exercises">
+                    <Row noGutters={true}>
+                        {this.state.exercises.map(exercise => {
+                            return (
+                                <Col md={4} key={exercise.id}>
+                                    <Card className="exercise-plan text-primary">
+                                        <Card.Body>
+                                            <div className="exercise-name">{exercise.name}</div>
+                                            {exercise.plan.split('--').map((set, indx) => {
+                                                return <div key={indx}>{set}</div>
+                                            })}   
+                                        </Card.Body>                         
+                                    </Card>
+                                </Col>
+                            )
+                        })}
+                    </Row>
+                </Container>
+            </Card>
+        )
+    }
+}
+
+export default Workout
